f66132310eda5f3f64464f29235e90f909f7f9c3
gomcodoctor <gomcodoctor@users.noreply.github.com>
2024-02-01 15:43:54 +0530

    Update USBPrinterService.kt
    updated to latest android version

--- android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/usb/USBPrinterService.kt
+++ android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/usb/USBPrinterService.kt
@@ -34,7 +34,22 @@ class USBPrinterService private constructor(private var mHandler: Handler?) {
             val action = intent.action
             if ((ACTION_USB_PERMISSION == action)) {
                 synchronized(this) {
-                    val usbDevice: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                    Log.v(LOG_TAG, "printing service is BroadcastReceiver")
+
+                    val usbDevice: UsbDevice? = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+                        intent.getParcelableExtra(UsbManager.EXTRA_DEVICE, UsbDevice::class.java)
+                    } else {
+                        intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                    }
+
+                    if (usbDevice != null) Log.v(
+                        LOG_TAG,
+                        "usbDevice for device: vendor_id: " + usbDevice.vendorId + ", product_id: " + usbDevice.productId
+                    )
+                    else {
+                        Log.v(LOG_TAG, "usbDevice is null")
+                    }
+
                     if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
                         Log.i(
                             LOG_TAG,
@@ -44,7 +59,7 @@ class USBPrinterService private constructor(private var mHandler: Handler?) {
                         state = STATE_USB_CONNECTED
                         mHandler?.obtainMessage(STATE_USB_CONNECTED)?.sendToTarget()
                     } else {
-                        Toast.makeText(context, mContext?.getString(R.string.user_refuse_perm) + ": ${usbDevice!!.deviceName}", Toast.LENGTH_LONG).show()
+                        Toast.makeText(context, mContext?.getString(R.string.user_refuse_perm) + ": ${usbDevice?.deviceName}", Toast.LENGTH_LONG).show()
                         state = STATE_USB_NONE
                         mHandler?.obtainMessage(STATE_USB_NONE)?.sendToTarget()
                     }
@@ -68,17 +83,18 @@ class USBPrinterService private constructor(private var mHandler: Handler?) {
     }
 
     fun init(reactContext: Context?) {
+        Log.v(LOG_TAG, "printing service is init")
         mContext = reactContext
         mUSBManager = mContext!!.getSystemService(Context.USB_SERVICE) as UsbManager
         mPermissionIndent = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-            PendingIntent.getBroadcast(mContext, 0, Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE)
+            PendingIntent.getBroadcast(mContext, 0, Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
         } else {
             PendingIntent.getBroadcast(mContext, 0, Intent(ACTION_USB_PERMISSION), 0)
         }
         val filter = IntentFilter(ACTION_USB_PERMISSION)
         filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED)
         mContext!!.registerReceiver(mUsbDeviceReceiver, filter)
-        Log.v(LOG_TAG, "ESC/POS Printer initialized")
+        Log.v(LOG_TAG, "ESC/POS printing service Printer initialized")
     }
 
     fun closeConnectionIfExists() {
@@ -104,6 +120,7 @@ class USBPrinterService private constructor(private var mHandler: Handler?) {
     fun selectDevice(vendorId: Int, productId: Int): Boolean {
 //        Log.v(LOG_TAG, " status usb ______ $state")
         if ((mUsbDevice == null) || (mUsbDevice!!.vendorId != vendorId) || (mUsbDevice!!.productId != productId)) {
+            Log.v(LOG_TAG, "mUsbDevice printing service is null")
             synchronized(printLock) {
                 closeConnectionIfExists()
                 val usbDevices: List<UsbDevice> = deviceList
@@ -120,6 +137,7 @@ class USBPrinterService private constructor(private var mHandler: Handler?) {
                 return false
             }
         } else {
+            Log.v(LOG_TAG, "mUsbDevice printing service is not null")
             mHandler?.obtainMessage(state)?.sendToTarget()
         }
 
