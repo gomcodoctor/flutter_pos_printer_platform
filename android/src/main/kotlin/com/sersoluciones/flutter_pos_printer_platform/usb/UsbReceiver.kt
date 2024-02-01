13b4d094cb7753ac27d2880cb5a09f1338174f02
gomcodoctor <gomcodoctor@users.noreply.github.com>
2024-02-01 15:43:27 +0530

    Update UsbReceiver.kt
    updated permissions

--- android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/usb/UsbReceiver.kt
+++ android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/usb/UsbReceiver.kt
@@ -8,17 +8,31 @@ import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.util.Log
 
+
 class UsbReceiver : BroadcastReceiver() {
+
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d("UsbReceiver", "Inside USB Broadcast action ${intent!!.action}")
 
         val action = intent.action
         if (UsbManager.ACTION_USB_DEVICE_ATTACHED == action) {
 
-            val usbDevice: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+            val usbDevice: UsbDevice? = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+                intent.getParcelableExtra(UsbManager.EXTRA_DEVICE, UsbDevice::class.java)
+            } else {
+                intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+            }
+
+            if (usbDevice != null) Log.v(
+                "UsbReceiver",
+                "usbDevic receive for device: vendor_id: " + usbDevice.vendorId + ", product_id: " + usbDevice.productId
+            )
+            else {
+                Log.v("UsbReceiver", "printing receive usbDevice is null")
+            }
 
             val mPermissionIndent = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-                PendingIntent.getBroadcast(context, 0, Intent("com.flutter_pos_printer.USB_PERMISSION"), PendingIntent.FLAG_IMMUTABLE)
+                PendingIntent.getBroadcast(context, 0, Intent("com.flutter_pos_printer.USB_PERMISSION"), PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
             } else {
                 PendingIntent.getBroadcast(context, 0, Intent("com.flutter_pos_printer.USB_PERMISSION"), 0)
             }
